9746e53c897ac8b3870e9a0bec92dd93b44f8221
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2025-04-24 17:51:52 -0300

    Update JogoDaForca.cs

--- JogoDaForca.cs
+++ JogoDaForca.cs
@@ -1,4 +1,4 @@
-namespace ConsoleApp3
+namespace JogoDaForca
 {
     internal class Program
     {
@@ -6,38 +6,87 @@ namespace ConsoleApp3
         {
             Console.WriteLine("\nJogo da Forca");
 
-            string palavra;
-            int chances = 5;
+            string palavra, entrada;
+            int tent = 5;
             int qtdLetras;
-            char letra;
+            char resposta;
 
-            palavra = "samuel";
+
+            palavra = "asdasdasd";
             qtdLetras = palavra.Length;
+
+            char[] letras = new char[qtdLetras];
+            List<char> letrasErradas = new List<char>();
+            List<char> letrasJaUsadas = new List<char>();
 
             Console.WriteLine("\nA palavra tem " + qtdLetras + " letras");
 
-            
-            for (int i = 0; i < palavra.Length; i++)
+            for (int i = 0; i < qtdLetras; i++)
             {
-                do
+                letras[i] = '_';
+            }
+
+            while (new string(letras) != palavra && tent > 0 )
+            {
+                Console.Clear();
+
+                Console.WriteLine("Palavra: " + string.Join(" ", letras) + "\n");
+                Console.WriteLine("Tentativas: " + tent);
+                Console.WriteLine("Letras erradas: " + string.Join(" ", letrasErradas));
+
+                Console.Write("\nDigite uma letra: ");
+                entrada = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(entrada) || entrada.Length != 1)
                 {
-                    Console.WriteLine("\nTentativas: " + chances);
-                    Console.Write("Digite a " + (i + 1) + "ª letra: ");
-                    letra = char.Parse(Console.ReadLine().ToLower());
-
-                    if (letra != palavra[i])
-                    {
-                        chances--;
-
-                        if (chances <= 0)
-                        {
-                            Console.WriteLine("As tentativas acabaram!");
-                            break;
-                        }
-                    }
-                    else continue;
+                    Console.WriteLine("\nDigite apenas uma letra!\nAperte qualquer tecla para tentar novamente...");
+                    Console.ReadKey();
+                    continue;
                 }
-                while (letra != palavra[i]);
+
+                resposta = entrada[0];
+
+                if (letrasJaUsadas.Contains(resposta))
+                {
+                    Console.WriteLine("\nVocê já tentou essa letra!\nAperte qualquer tecla para tentar novamente...");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                bool letraValida = false;
+
+                for (int i = 0; i < qtdLetras; i++)
+                {
+                    if (resposta == palavra[i])
+                    {
+                        letras[i] = resposta;
+                        letraValida = true;
+                        letrasJaUsadas.Add(resposta);
+                    }
+                }
+                if (letraValida == false)
+                {
+                    Console.WriteLine("\nLetra errada!\nAperte qualquer tecla para tentar novamente...");
+                    Console.ReadKey();
+                    letrasErradas.Add(resposta);
+                    letrasJaUsadas.Add(resposta);
+                    tent--;
+                }
+            }
+            if (tent <= 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Palavra: " + string.Join(" ", letras));
+                Console.WriteLine("Tentativas: " + tent);
+                Console.WriteLine("\nAs tentativas acabaram!\nPerdeu!");
+            }
+            else
+            {
+                Console.Clear();
+
+                Console.WriteLine("Palavra: " + string.Join(" ", letras));
+                Console.WriteLine("Tentativas: " + tent);
+                Console.WriteLine("\nParabéns!");
             }
         }
     }
