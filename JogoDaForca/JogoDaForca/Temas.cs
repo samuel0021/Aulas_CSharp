04e7386c702f37854d2ee5bd45cfd86058cdb76b
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2025-05-05 20:27:11 -0300

    Update Temas.cs

--- JogoDaForca/JogoDaForca/Temas.cs
+++ JogoDaForca/JogoDaForca/Temas.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,37 +11,75 @@ namespace JogoDaForca
         public static List<string> comidas = new List<string>
         {
             "abacaxi", "cenoura", "morango", "pizza", "chocolate",
-            "lasanha", "hamburguer", "maca"
+            "lasanha", "hamburguer", "maca", "pudim", "torta",
+            "pastel", "salada", "sushi", "cachorro-quente", "arroz",
+            "feijao", "lasanha", "panqueca", "bife", "farofa"
         };
 
         public static List<string> lugares = new List<string>
         {
             "praia", "montanha", "deserto", "floresta", "cidade",
-            "ilha", "caverna", "lago"
+            "ilha", "caverna", "lago", "campo", "selva",
+            "vila", "fazenda", "castelo", "parque",
+            "pico", "palmital", "rua", "estrada"
         };
 
         public static List<string> animais = new List<string>
         {
             "cachorro", "elefante", "gato", "papagaio", "cavalo",
-            "tubarao", "coruja"
+            "tubarao", "coruja", "girafa", "leao", "tigre",
+            "urso", "camelo", "coelho", "ovelha", "macaco",
+            "zebra", "galo", "galinha", "pato"
         };
 
         public static List<string> objetos = new List<string>
         {
             "computador", "cadeira", "violino", "sapato", "relogio",
-            "bicicleta", "janela", "aviao"
+            "bicicleta", "janela", "aviao", "mesa", "lampada",
+            "pente", "telefone", "teclado", "microfone", "livro",
+            "caneta", "borracha", "papel", "faca", "copo"
         };
 
         public static List<string> espaco = new List<string>
         {
             "estrela", "planeta", "meteoro", "cometa", "galaxia",
-            "sol", "lua", "nuvem"
+            "sol", "lua", "nuvem", "buraco negro", "satélite",
+            "asteroide", "via lactea", "sistema solar", "luz",
+            "radiação", "cosmos", "galáxia espiral", "universo",
+            "exoplaneta", "astro"
         };
 
         public static List<string> extras = new List<string>
         {
             "telefone", "livro", "chave", "foguete", "gramado",
-            "mapa"
+            "mapa", "caneta", "oculos", "espelho", "boné",
+            "tacape", "capinha", "boneca", "relógio", "puzzle",
+            "carta", "dado", "carro", "cesta", "bola"
         };
+
+        public static List<string> tecnologia = new List<string>
+        {
+            "computador", "smartphone", "tablet", "relógio inteligente", "ferramenta",
+            "programa", "internet", "wi-fi", "redes", "servidor"
+        };
+
+        public static List<string> transportes = new List<string>
+        {
+            "carro", "bicicleta", "avião", "trem", "navio",
+            "ônibus", "helicóptero", "moto", "caminhão", "cavalo"
+        };
+
+        public static List<string> cores = new List<string>
+        {
+            "azul", "verde", "vermelho", "amarelo", "preto",
+            "branco", "roxo", "laranja", "rosa", "cinza"
+        };
+
+        public static List<string> instrumentosMusicais = new List<string>
+        {
+            "violão", "guitarra", "bateria", "piano", "flauta",
+            "saxofone", "trompete", "harpa", "baixo", "percussão"
+        };
+
     }
 }
