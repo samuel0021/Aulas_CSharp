48fc6965716b7a858b9e1a6400d1b8446f101de5
samuel0021 <139814578+samuel0021@users.noreply.github.com>
2025-05-05 23:58:26 -0300

    Update Program.cs

--- JogoDaForca/JogoDaForca/Program.cs
+++ JogoDaForca/JogoDaForca/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -8,8 +8,28 @@ namespace JogoDaForca
     {
         static void Main(string[] args)
         {
-            JogoDaForca jogo = new JogoDaForca();
-            jogo.Iniciar();
+            JogoDaForca jogo = new JogoDaForca();            
+
+            bool jogarNovamente = true;
+
+            while (jogarNovamente)
+            {
+                jogo.Iniciar();
+
+                Console.WriteLine("\n\nDeseja jogar novamente? \n\n - Sim ( 1 )\n - Não ( 2 )");
+                Console.Write("\nOpção: ");
+                int resposta;
+
+                while (!int.TryParse(Console.ReadLine(), out resposta) || resposta < 1 || resposta > 2)
+                {
+                    JogoDaForca.Mensagem("Digite apenas 1 ou 2!");
+                }
+
+                jogarNovamente = (resposta == 1);
+                Console.Clear();
+            }
+
+            Console.WriteLine("Jogo encerrado.");
         }
     }
 }
